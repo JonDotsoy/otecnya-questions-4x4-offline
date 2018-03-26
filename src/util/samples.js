1f1ea67f789c5c3c1dc538169166f5bdb423c7c4
Jonathan Delgado Zamorano <hi@jon.soy>
2018-03-26 18:52:35 -0300

    Update function sampleQuestions

--- src/util/samples.js
+++ src/util/samples.js
@@ -1,4 +1,5 @@
 
+const random = require('lodash/random')
 const sample = require('lodash/sample')
 
 module.exports.formatQuestion = formatQuestion
@@ -22,17 +23,5 @@ function formatQuestion ({stag, options, title, sort}) {
 }
 
 function sampleQuestions (questions, len = 10) {
-  const outsamples = []
-
-  while (outsamples.length < 10) {
-    const proposalSample = formatQuestion(sample(questions))
-
-    const o = outsamples.find(e => e.title === proposalSample.title)
-
-    if (o === undefined) {
-      outsamples.push(proposalSample)
-    }
-  }
-
-  return outsamples
+  return questions.concat().sort(() => random(-1, 1)).slice(0, len).map(formatQuestion)
 }
